3205f07e89d94266e7c428225fff5a744b48b50d
Brad McGarvey <45929861+bemcgarvey@users.noreply.github.com>
2021-05-11 14:30:54 -0600

    Initial window and classes

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setOrganizationName("QES");
+    QApplication::setApplicationName("2048");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
